dfd8b317fd7a10eb08e186303f2064796aa849d9
unknown <pratyakshsinghal635@gmail.com>
2026-07-16 20:09:00 +0530

    data ingested

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    # Load environment variables from .env file
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_MODEL = "llama-3.3-70b-versatile"
+    GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+    QDRANT_COLLECTION = "enterprise_rag_collection"
+
+
+settings = Settings()
